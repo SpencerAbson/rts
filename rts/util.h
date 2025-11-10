6bc63593cd8eb1fb6f13261754b496a3dee8f054
Spencer Abson <sabson.oss@gmail.com>
2025-11-10 00:33:54 +0000

    util: Define rts_malloc and checking assertions
    Hello, world.  We define rts_malloc with the hope of using it
    in a custom allocator down the line, and introduce 'checking'
    assertions as a sanity measure for debugging.

---
+++ rts/util.h
@@ -0,0 +1,29 @@
+#ifndef UTIL_H_
+#define UTIL_H_
+
+#include <cassert>
+#include <iostream>
+
+#ifdef EN_CHECKING_ASSERT
+#define rts_checking_assert(EXPR)		\
+    assert (EXPR)
+#else
+#define rts_checking_assert(EXPR)		\
+    ((void)(0 && EXPR))
+#endif
+
+inline void*
+rts_malloc (size_t size)
+{
+  void *ret = malloc (size);
+  if (!ret)
+    {
+      std::cerr << "rts: (malloc) failed to allocate " << size
+		<< " bytes.\n";
+      abort ();
+    }
+
+  return ret;
+}
+
+#endif // UTIL_H_
