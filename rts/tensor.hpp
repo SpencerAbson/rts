109952564bae2ba861c36fbe8cc745e7966f45e5
Spencer Abson <sabson.oss@gmail.com>
2025-11-24 21:59:43 +0000

    [tensor] improve constructor interface
    Allow shape to be defined via std::initializer_list, optional initial
    fill values, and add sanity checking assertions.

--- rts/tensor.hpp
+++ rts/tensor.hpp
@@ -4,25 +4,62 @@
 #include <stdint.h>
 #include <vector>
 #include <numeric>
+#include "util.h"
 
 /* Generic tensor wrapper.  */
 template <typename T>
 class tensor
 {
 public:
-  tensor (std::vector<uint32_t> shape)
-    : shape (shape), stride (shape.size ())
+
+  tensor (std::vector<T> data, std::vector<uint32_t> shape)
+    : vec (data),
+      shape (shape),
+      stride (shape.size ())
   {
-    vec.resize (std::reduce (shape.begin (), shape.end (), 1,
-			     std::multiplies ()));
+    rts_checking_assert (std::reduce (shape.begin (), shape.end (), 1,
+				      std::multiplies ())
+			 == vec.size ());
     init_stride (0);
-  };
+  }
+
+  tensor (std::vector<T> data, std::initializer_list<uint32_t> shape)
+    : vec (data),
+      shape (shape),
+      stride (shape.size ())
+  {
+    rts_checking_assert (std::reduce (shape.begin (),shape.end (), 1,
+				      std::multiplies ())
+			 == vec.size ());
+    init_stride (0);
+  }
+
+  tensor (std::vector<uint32_t> shape, T init=T{})
+    : vec (std::reduce (shape.begin (), shape.end (), 1,
+			std::multiplies ()), init),
+      shape (shape),
+      stride (shape.size ())
+  {
+    init_stride (0);
+  }
+
+  tensor (std::initializer_list<uint32_t> shape, T init=T{})
+    : vec (std::reduce (shape.begin (), shape.end (), 1,
+			std::multiplies ()), init),
+      shape (shape),
+      stride (shape.size ())
+  {
+    init_stride (0);
+  }
+
+  tensor () = default;
 
   std::vector<T> vec;
   std::vector<uint32_t> shape;
   std::vector<uint32_t> stride;
 
 private:
+
   uint32_t
   init_stride (uint32_t i)
   {
