33c6bac9fdc2e0495233b2257dcc6472273ecfbc
Spencer Abson <sabson.oss@gmail.com>
2026-01-15 20:03:11 +0000

    [linear_lif] Refactor access pattern
    After heavy profiling, it became obvious that the somewhat random
    access pattern used here is actually bad for performance!
    
    This is faster in almost all cases, far more predictable, and the
    difference between the two implementations is a good case study
    for the report (stalled backend cycles, cache misses...).

--- rts/layers/linear_lif.hpp
+++ rts/layers/linear_lif.hpp
@@ -38,93 +38,81 @@ public:
 
   uint32_t output_size () const { return m_num_outputs; }
 
+  /* It's difficult to parallelise this update given the binary COO encoded
+     SPIKES_IN buffer because NEON lacks gather-loads/scatter-stores.
+
+     The approach taken here is vectorize along the columns of the weight
+     matrix, which gives a highly predictable access pattern.  */
   std::vector<uint32_t>
   timestep_batched (const std::vector<uint32_t> &spikes_in,
 		    uint32_t batch_begin, uint32_t batch_end)
   {
-    std::vector<uint32_t> spikes_out;
-
-    float32x4_t beta_splat = vdupq_n_f32 (m_beta);
-    float32x4_t thresh_splat = vdupq_n_f32 (m_v_thresh);
-
-    /* It's difficult to parallelise this update given the binary COO encoded
-       input buffer because NEON lacks gather-loads/scatter-stores.
-
-       The approach taken here is vectorize along the columns of the weight
-       matrix (and therefore the layer size), linearly processing each input
-       spike across a 'slice' of values.
-
-       A minor headache is that we have to scalarize the conditional pushes
-       to the output buffer, but this seems to incur negligable overhead.  */
+    /* Limit before epilogue for a VF of 4.  */
     uint32_t max_neon = batch_end & ~0x03;
-    for (uint32_t i = batch_begin; i < max_neon; i+=4)
+    /* Vectorised constants for dynamics.  */
+    const float32x4_t beta_splat   = vdupq_n_f32 (m_beta);
+    const float32x4_t thresh_splat = vdupq_n_f32 (m_v_thresh);
+
+    /* Handle the neuron's dynamics and linear bias.  */
+    float32x4_t membrane, bias;
+    for (uint32_t i = 0; i < max_neon; i+=4)
       {
-	/* SLICE holds one value for each column that we're processing.  */
-	float32x4_t slice = vld1q_f32 (&m_bias[i]);
-	for (uint32_t j  = 0; j < spikes_in.size (); j++)
-	  {
-	    rts_checking_assert (spikes_in[j] < m_weights.shape[0]);
-	    float32x4_t weight_slice
-	      = vld1q_f32 (&m_weights.vec[i + spikes_in[j]
-					  * m_weights.stride[0]]);
+	bias     = vld1q_f32 (&m_bias[i]);
+	membrane = vld1q_f32 (&m_v_membrane[i]);
 
-	    slice = vaddq_f32 (slice, weight_slice);
-	  }
-
-	/* The membrane potentials of the the neurons associated with each
-	   value in SLICE.  */
-	float32x4_t membrane = vld1q_f32 (&m_v_membrane[i]);
 	/* Conditionally set a soft reset value (actually float).  */
 	uint32x4_t reset = vandq_u32 (vcgtq_f32 (membrane, thresh_splat),
 				      vreinterpretq_u32_f32 (thresh_splat));
+	/* Compute bias + (mem * beta) - ?reset.  */
+	membrane = vfmaq_f32 (bias, membrane, beta_splat);
+	membrane = vsubq_f32 (membrane, vreinterpretq_f32_u32 (reset));
 
-	/* Compute (mem * beta) + weighted_spk_in - ?reset.  */
-	slice = vfmaq_f32 (slice, membrane, beta_splat);
-	slice = vsubq_f32 (slice, vreinterpretq_f32_u32 (reset));
-
-	/* Compare the new potentials to V_THRESH.  */
-	uint32x4_t cmp = vcgtq_f32 (slice, thresh_splat);
-	/* Now scalarize the conditional pushes to SPIKES_OUT.  This code
-	   is verbose because the 'lane' arguments to vget/vset must be
-	   integer literals, but I don't think it warrants a macro.  */
-	if (vgetq_lane_u32 (cmp, 0) != 0)
-	  spikes_out.push_back (i);
-	if (vgetq_lane_u32 (cmp, 1) != 0)
-	  spikes_out.push_back (i+1);
-	if (vgetq_lane_u32 (cmp, 2) != 0)
-	  spikes_out.push_back (i+2);
-	if (vgetq_lane_u32 (cmp, 3) != 0)
-	  spikes_out.push_back (i+3);
-
-	vst1q_f32 (&m_v_membrane[i], slice);
+	vst1q_f32 (&m_v_membrane[i], membrane);
       }
     /* Scalar epilogue.  */
     for (uint32_t i = max_neon; i < batch_end; i++)
       {
-	float update = m_bias[i];
-	for (uint32_t j = 0; j < spikes_in.size (); j++)
-	  {
-	    rts_checking_assert (spikes_in[j] < m_weights.shape[0]);
-	    update += m_weights.vec[i + spikes_in[j] * m_weights.stride[0]];
-	  }
-
-	update = m_v_membrane[i] * m_beta + update;
+	float update = m_bias[i] + (m_v_membrane[i] * m_beta);
 	if (m_v_membrane[i] > m_v_thresh)
 	  update -= m_v_thresh;
-
-	if (update > m_v_thresh)
-	  spikes_out.push_back (i);
 
 	m_v_membrane[i] = update;
       }
 
-    return spikes_out;
+    /* Handle the spiking input.  */
+    for (uint32_t spike : spikes_in)
+      {
+	rts_checking_assert (spike < m_weights.shape[0]);
+	for (uint32_t i = 0; i < max_neon; i+=4)
+	  {
+	    float32x4_t weight_slice
+	      = vld1q_f32 (&m_weights.vec[i + spike * m_weights.stride[0]]);
+
+	    membrane = vld1q_f32 (&m_v_membrane[i]);
+	    membrane = vaddq_f32 (membrane, weight_slice);
+
+	    vst1q_f32 (&m_v_membrane[i], membrane);
+	  }
+	/* Scalar epilogue.  */
+	for (uint32_t i = max_neon; i < batch_end; i++)
+	  m_v_membrane[i] += m_weights.vec[i + spike * m_weights.stride[0]];
+      }
+
+    std::vector<uint32_t> spike_out;
+    /* Push the spiking neurons to SPIKE_OUT.  */
+    for (uint32_t i = 0; i < batch_end; i++)
+      {
+	if (m_v_membrane[i] > m_v_thresh)
+	  spike_out.push_back (i);
+      }
+
+    return spike_out;
   }
 
   std::vector<uint32_t>
   timestep (const std::vector<uint32_t> &spikes_in)
   {
-    return timestep_batched (spikes_in, 0, m_weights.shape[1]);
+    return timestep_batched (spikes_in, 0, m_num_outputs);
   }
 
   void
